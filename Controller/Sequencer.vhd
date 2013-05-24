984e0c6b026cd089dd467218ae9fb89483d5f671
Robert Mills <propellerhead.001@googlemail.com>
2013-05-24 15:48:07 +0100

    Sequencer State Machine
    the state machine has been finished for the sequencer, it is designed to
    stall the process in the event of a branch instruction

--- Controller/Sequencer.vhd
+++ Controller/Sequencer.vhd
@@ -22,7 +22,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -36,13 +36,33 @@ entity Sequencer is
 			  Branch : in STD_LOGIC;
 			  Jump : in STD_LOGIC;
 			  clk : in STD_LOGIC;
+			  rst : in STD_LOGIC;
            Address : out  STD_LOGIC_VECTOR (7 downto 0));
 end Sequencer;
 
 architecture Behavioral of Sequencer is
-
+type state_type is (count, jump, branch, eval_branch);
+signal state : state_type;
+signal instruction : integer;
 begin
-
+process(clk, rst)
+begin
+	if (rising_edge(clk)) then
+	if (rst = '1') then
+		state <= count;
+	else
+		if ((state = count)and (jump = '1') and (break = '0'))then
+			state <= jump;
+		elsif((state = count)and (jump = '0') and (break = '1'))then
+			state <= break;
+		elsif(state = branch)then
+			state <= eval_branch;
+		elsif((state = eval_branch) or (state = jump)) then
+			satte <= count;
+		end if;
+	end if;
+	end if;
+end process;
 
 end Behavioral;
 
